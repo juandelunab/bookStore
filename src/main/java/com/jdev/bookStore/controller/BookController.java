9979442db61a93c14812d9669c1e96f39e0dc2c6
juandelunab <juan.delunabeltre@cvshealth.com>
2022-06-14 13:28:11 -0600

    initial commit

---
+++ src/main/java/com/jdev/bookStore/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.jdev.bookStore.controller;
+
+import com.jdev.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+        BookDto book = BookDto.builder().title(" my first book title").build();
+
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+
+
+    }
+
+}
